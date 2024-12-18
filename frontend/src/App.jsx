2c636c3d7be1b2add23b407cf4f6425fdc34c537
haru-student <nagaharu0307@outlook.jp>
2024-12-18 18:33:08 +0900

    ヘッダー追加

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import Home from './Home'
+import Header from './Header'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +10,7 @@ function App() {
   return (
     <>
       <div class="App">
+        <Header />
         <h1>ログインを実装しようね</h1>
         <Home />
       </div>
