3d1d77f4ec137c672bf427f68beb6816956f49eb
haru-student <nagaharu0307@outlook.jp>
2024-12-20 03:46:25 +0900

    edit header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,16 @@ import './App.css'
 import Home from './Home'
 import Header from './Header'
 import Footer from './Footer';
+import {auth} from "./firebase";
+import {useAuthState} from "react-firebase-hooks/auth";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user] = useAuthState(auth);
 
   return (
     <>
       <div>
-        <Header />
+        <Header user={user} />
         <Home />
         <Footer />
       </div>
--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,52 +1,10 @@
 import { signInWithPopup } from 'firebase/auth';
-import React from 'react'
 import {auth, provider} from "./firebase";
-import {useAuthState} from "react-firebase-hooks/auth";
-import Container from 'react-bootstrap/Container';
 
-function Login() {
-    const [user] = useAuthState(auth);
-    return (
-        <Container className="text-center">
-            {user ? (
-            <div>
-                <UserInfo />
-                <SignOutButton />
-            </div>
-        ) : (
-            <SignInButton />
-        )}
-        </Container>
-    )
+export function signIn() {
+    signInWithPopup(auth, provider);
 }
 
-export default Login
-
-function SignInButton() {
-    const signInWithGoogle = () => {
-        signInWithPopup(auth, provider);
-    };
-    return (
-    <button onClick={signInWithGoogle}>
-        <p>サインイン</p>
-    </button>
-    )
-}
-
-function SignOutButton() {
-    return (
-    <button onClick={() => auth.signOut()}>
-        <p>サインアウト</p>
-    </button>
-    )
-}
-
-function UserInfo(){
-    return (
-        <div className = "userInfo">
-            <img src={auth.currentUser.photoURL} alt="" />
-            <p>{auth.currentUser.displayName}</p>
-        </div>
-    )
-
+export function signOut() {
+    auth.signOut();   
 }
